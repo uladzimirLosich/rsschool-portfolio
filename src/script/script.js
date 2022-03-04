cdf5aa0885f021f5c2bb63bffd6db5b3e21c9c29
Uladzimir Losich <uladzimir_lsch@hotmail.com>
2022-03-04 12:21:50 +0300

    feat: add close menu function

--- src/script/script.js
+++ src/script/script.js
@@ -1,12 +1,29 @@
-const hamburger = document.querySelector('.hamburger__menu')
+//----- Open Menu -----//
+
+const toggleHamburger = document.querySelector('.hamburger__menu')
 const mobileMenu = document.querySelector('.hamburger__nav')
 const mobileBackground = document.querySelector('.hamburger__background')
 
-const isToggleMenu = () => {
-    hamburger.classList.toggle('open')
+const openMenu = () => {
+    toggleHamburger.classList.toggle('open')
     mobileMenu.classList.toggle('open')
     mobileBackground.classList.toggle('open')
 }
-hamburger.addEventListener('click', isToggleMenu)
 
+toggleHamburger.addEventListener('click', openMenu)
+
+//----- Close Menu -----//
+
+const toggleNavLinks = document.querySelector('.hamburger__nav-list')
+
+const closeMenu = (ev) => {
+    console.log(ev)
+    if (ev.target.classList.contains('nav-link')) {
+        toggleHamburger.classList.remove('open')
+        mobileMenu.classList.remove('open')
+        mobileBackground.classList.remove('open')
+    }
+}
+
+toggleNavLinks.addEventListener('click', closeMenu)
 
