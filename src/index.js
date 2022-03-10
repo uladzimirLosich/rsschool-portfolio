aa90c62359fe1549cf7bc0597851835736162675
Uladzimir Losich <uladzimir_lsch@hotmail.com>
2022-03-10 21:14:52 +0300

    feat: add data-attribute to index.html & add translate.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import i18Obj from "./modules/translate.js";
+
 //---------- OPEN MENU ----------//
 
 const toggleHamburger = document.querySelector('.hamburger__menu')
@@ -41,25 +43,24 @@ preloadImages()
 //---------- CHANGE OF SEASONS ----------//
 
 const portfolioBtns = document.querySelector('.portfolio__btns')
-const portfolioImage = document.querySelectorAll('.portfolio__item')
 
 const changeItem = (e) => {
+    const portfolioImage = document.querySelectorAll('.portfolio__item')
     if (e.target.dataset.season) {
-        portfolioImage.forEach((img, i) => img.src = `./src/assets/img/${e.target.dataset.season}/${i + 1}.jpg`)
+        [...portfolioImage].forEach((img, i) => img.src = `./src/assets/img/${e.target.dataset.season}/${i + 1}.jpg`)
     }
 }
 portfolioBtns.addEventListener('click', changeItem)
 
 //---------- ACTIVE BUTTON ----------//
 
-const portfolioBtnsActive = document.querySelectorAll('.btn_non-colored')
-
 const changeActiveClass = (e) => {
+    const portfolioBtnsActive = document.querySelectorAll('.btn_non-colored')
     if (e.target) {
-        [...portfolioBtnsActive].forEach((i) => i === e.target && i.classList.add('active'))
-    }
-    {
-        [...portfolioBtnsActive].forEach((i) => i !== e.target && i.classList.remove('active'))
+        [...portfolioBtnsActive].forEach((i) => {
+            i === e.target && i.classList.add('active')
+            i !== e.target && i.classList.remove('active')
+        })
     }
 }
 portfolioBtns.addEventListener('click', changeActiveClass)
@@ -67,15 +68,30 @@ portfolioBtns.addEventListener('click', changeActiveClass)
 //---------- ACTIVE LANG-SWITCH ----------//
 
 const langSwitch = document.querySelector('.lang-switcher')
-const langSwitchActive = document.querySelectorAll('.lang-link')
 
 const changeActiveLink = (e) => {
+    const langSwitchActive = document.querySelectorAll('.lang-link')
     if (e.target) {
-        [...langSwitchActive].forEach((i) => i === e.target && i.classList.add('active'))
-    }
-    {
-        [...langSwitchActive].forEach((i) => i !== e.target && i.classList.remove('active'))
+        [...langSwitchActive].forEach((i) => {
+            i === e.target && i.classList.add('active')
+            i !== e.target && i.classList.remove('active')
+        })
     }
 }
 langSwitch.addEventListener('click', changeActiveLink)
+
+//---------- SWITCH LANGUAGES ----------//
+
+const switchLang = document.querySelector('.lang-link')
+
+const getTranslate = (ru) => {
+    const changeLang = document.querySelectorAll('[data-i18n]')
+    changeLang.forEach((current) => {
+        if (current.dataset.i18n === i18Obj['ru'][''] && current.placeholder) {
+            current.placeholder = i18Obj['ru']
+            current.textContent = ''
+        }
+    })
+}
+switchLang.addEventListener('click', getTranslate)
 
