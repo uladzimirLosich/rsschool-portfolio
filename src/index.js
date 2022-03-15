766ddfd399770427ce4ff4639414b615202a664f
Uladzimir Losich <uladzimir_lsch@hotmail.com>
2022-03-15 19:11:35 +0300

    refactor: correcting function

--- src/index.js
+++ src/index.js
@@ -1,32 +1,37 @@
 // import i18Obj from "./modules/translate.js";
 
-//---------- MENU ----------//
-
-const openMenu = document.querySelector('.hamburger__menu')
-const mobileMenu = document.querySelector('.hamburger__nav')
-const mobileBackground = document.querySelector('.hamburger__background')
-
-openMenu.onclick = () => {
-    openMenu.classList.toggle('open')
-    mobileMenu.classList.toggle('open')
-    mobileBackground.classList.toggle('open')
+window.onload = () => {
+    openMenu()
+    closeMenu()
+    preloadImages()
+    portfolioBtns()
+    langSwitch()
+    themeToggle()
 }
 
-const closeMenu = document.querySelector('.hamburger__nav-list')
-
-closeMenu.onclick = (e) => {
-    if (e.target.classList.contains('nav-link')) {
-        openMenu.classList.remove('open')
-        mobileMenu.classList.remove('open')
-        mobileBackground.classList.remove('open')
-    }
+const openMenu = () => {
+    document.querySelector('.hamburger__menu').addEventListener('click', () => {
+        const openMenuItems = document.querySelectorAll('.hamburger__menu, .hamburger__nav, .hamburger__background')
+        openMenuItems.forEach((event) => {
+            event.classList.toggle('open')
+        })
+    })
 }
 
-//---------- IMAGE CASHING ----------//
-
-const seasons = ['winter', 'spring', 'summer', 'autumn'];
+const closeMenu = () => {
+    document.querySelector('.hamburger__nav-list').addEventListener('click', (event) => {
+            const closeMenuItems = document.querySelectorAll('.hamburger__menu, .hamburger__nav, .hamburger__background')
+            if (event.target.classList.contains('nav-link')) {
+                closeMenuItems.forEach((event) => {
+                    event.classList.remove('open')
+                })
+            }
+        }
+    )
+}
 
 const preloadImages = () => {
+    const seasons = ['winter', 'spring', 'summer', 'autumn'];
     for (let i = 0; i <= seasons.length - 1; i++) {
         for (let j = 1; j <= 6; j++) {
             const img = new Image()
@@ -34,38 +39,46 @@ const preloadImages = () => {
         }
     }
 }
-preloadImages()
 
-//---------- CHANGE OF SEASONS ----------//
-
-const portfolioBtns = document.querySelector('.portfolio__btns')
-
-portfolioBtns.onclick = (e) => {
-    const portfolioImage = document.querySelectorAll('.portfolio__item')
-    if (e.target.dataset.season) {
-        [...portfolioImage].forEach((img, i) => img.src = `./src/assets/img/${e.target.dataset.season}/${i + 1}.jpg`)
-    }
-    const portfolioBtnsActive = document.querySelectorAll('.btn_non-colored')
-    if (e.target) {
-        [...portfolioBtnsActive].forEach((i) => {
-            i === e.target && i.classList.add('active')
-            i !== e.target && i.classList.remove('active')
-        })
-    }
+const portfolioBtns = () => {
+    document.querySelector('.portfolio__btns').addEventListener('click', (event) => {
+            const portfolioImage = document.querySelectorAll('.portfolio__item')
+            if (event.target.dataset.season) {
+                [...portfolioImage].forEach((img, i) => img.src = `./src/assets/img/${event.target.dataset.season}/${i + 1}.jpg`)
+            }
+            const portfolioBtnsActive = document.querySelectorAll('.btn_non-colored')
+            if (event.target) {
+                [...portfolioBtnsActive].forEach((i) => {
+                    i === event.target && i.classList.add('active')
+                    i !== event.target && i.classList.remove('active')
+                })
+            }
+        }
+    )
 }
 
-//---------- ACTIVE LANG-SWITCH ----------//
+const langSwitch = () => {
+    document.querySelector('.lang-switcher').addEventListener('click', (event) => {
+            const langSwitchActive = document.querySelectorAll('.lang-link')
+            if (event.target) {
+                [...langSwitchActive].forEach((i) => {
+                    i === event.target && i.classList.add('active')
+                    i !== event.target && i.classList.remove('active')
+                })
+            }
+        }
+    )
+}
 
-const langSwitch = document.querySelector('.lang-switcher')
-
-langSwitch.onclick = (e) => {
-    const langSwitchActive = document.querySelectorAll('.lang-link')
-    if (e.target) {
-        [...langSwitchActive].forEach((i) => {
-            i === e.target && i.classList.add('active')
-            i !== e.target && i.classList.remove('active')
+const themeToggle = () => {
+    const theme = document.querySelector('.theme-toggle')
+    theme.addEventListener('click', () => {
+        const themes = document.querySelectorAll('.section-skills, .section-portfolio, .section-video, .section-price, .price__item, .section-heading, .section-heading__title, .btn_non-colored')
+        themes.forEach((event) => {
+            event.classList.toggle('light-theme')
         })
-    }
+        theme.classList.toggle('light')
+    })
 }
 
 // //---------- SWITCH LANGUAGES ----------//
@@ -82,16 +95,15 @@ langSwitch.onclick = (e) => {
 //         })
 // })
 
-const theme = document.querySelector('.theme-toggle')
-const themes = document.querySelectorAll('.section-skills, .section-portfolio, .section-video, .section-price, .price__item, .section-heading, .section-heading__title, .btn_non-colored')
+let theme = 'light'
 
-theme.onclick = () => {
-    theme.classList.toggle('toggle')
-    themes.forEach((e) => {
-        e.classList.toggle('light-theme')
-    })
-}
+window.addEventListener('beforeunload', () => {
+    localStorage.setItem('theme', theme)
+})
 
-
-
-
+window.addEventListener('load', () => {
+    if (localStorage.getItem('theme')) {
+        const themeMode = localStorage.getItem('theme')
+        themeToggle(themeMode)
+    }
+})
